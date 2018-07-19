c114472fe224a3d9ff00f66891c50a98603a2f46
MasihNoob <neraka.malam@gmail.com>
2018-07-19 23:29:39 +0700

    fixing project tahap satu

--- app/src/main/java/com/lunartech/lovelymalang/BaseActivity.java
+++ app/src/main/java/com/lunartech/lovelymalang/BaseActivity.java
@@ -3,18 +3,18 @@ package com.lunartech.lovelymalang;
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class BaseActivity extends ActionBarActivity {
+public class BaseActivity extends AppCompatActivity {
 
     @SuppressLint("InlinedApi")
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
-        super.onCreate(savedInstanceState);
 
     }
 
